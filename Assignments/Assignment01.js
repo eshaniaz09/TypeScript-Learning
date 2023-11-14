2dc01ec476d52ba897360b283be882268cb12b5c
Eesha Niaz <eshaniaz5@gmail.com>
2023-11-14 16:48:42 +0500

    Assignments

---
+++ Assignments/Assignment01.js
@@ -0,0 +1,21 @@
+"use strict";
+// 1- Declare variables for a person's name, age, and whether they are a student, Print these variables to the console.
+var named = "Eesha Niaz"; // Inferring Types, 
+//take your cursor on the variable name (var named: string)
+console.log(named);
+var age = 19; // Inferring Types, 
+//take your cursor on the variable name (var age: number)
+console.log(age);
+var student = true; // Inferring Types, 
+//take your cursor on the variable name (var named: boolean)
+console.log(student);
+// 2- Declare two variables with numeric values, Perform addition, subtraction, multiplication, and division on these variables, Print the results to the console.
+var num1 = 20;
+var num2 = 10;
+console.log(num1 + num2); // output: 30
+console.log(num1 - num2); // output: 10
+console.log(num1 * num2); // output: 200
+console.log(num1 / num2); // output: 2
+// 3- Declare a string variable with your favorite programming language, Print the final sentence to the console.
+var lang = "TypeScript";
+console.log(`${lang} is a language that extends JavaScript by adding static typing and other features that is more readable and maintainable then javascript`);
