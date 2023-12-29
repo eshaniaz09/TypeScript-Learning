bd41403c00b7607a24b6d6468bfdac195d708205
Eesha Niaz <eshaniaz5@gmail.com>
2023-12-29 11:35:05 +0500

    Arrays

---
+++ 08-Scope.js
@@ -0,0 +1,14 @@
+"use strict";
+// There are two types of scope :
+//global Scope variable
+//block scope variable
+//let
+//const
+//var
+let a = 'Esha';
+{
+    a = 'hello';
+    // let a: string = "niaz";
+    console.log(a);
+}
+console.log(a);
---
+++ 08-Scope.ts
@@ -0,0 +1,14 @@
+// There are two types of scope :
+//global Scope variable
+//block scope variable
+//let
+//const
+//var
+let a: string = 'Esha';
+{
+    a = 'hello';
+    // let a: string = "niaz";
+    console.log(a);
+    
+}
+console.log(a);
