76a58abe86500e9b62dba0fff5e5ef350d13ea74
Eesha Niaz <eshaniaz5@gmail.com>
2024-01-07 23:29:22 +0500

    intro

--- 01-intro.js
+++ 01-intro.js
@@ -1,4 +1,6 @@
 "use strict";
+//typescript is used as a strict type of javascript.
+//typescript is more accurate then simple javascript 
 console.log("hello esha");
 console.log("dev esha 😀");
 console.log("hello its me");
