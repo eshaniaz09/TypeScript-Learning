244a53aeb729c251879ec7bb87650251a66e1226
Eesha Niaz <eshaniaz5@gmail.com>
2024-01-21 00:47:57 +0500

    Assignment07

--- Assignments/Assignment07.js
+++ Assignments/Assignment07.js
@@ -1,4 +1,8 @@
+// Develop a ts calculator application which may perform basic arithmetic operations and take input from users using inquirer
+// Note: The output must be shown in different colors for each output of arithmetic operation.
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.gray.bold.italic.underline('Calculator'));
 const answer = await inquirer.prompt([
     {
         type: 'number',
@@ -8,7 +12,7 @@ const answer = await inquirer.prompt([
     {
         type: 'number',
         name: 'num2',
-        message: 'Enter the second number'
+        message: 'Enter the second number: '
     },
     {
         type: 'list',
@@ -21,18 +25,30 @@ let result;
 switch (answer.operator) {
     case '+':
         result = answer.num1 + answer.num2;
-        console.log('The answer for addition is: ', result);
+        console.log(chalk.yellow('The answer for addition is: '), chalk.yellow(result));
         break;
     case '-':
         result = answer.num1 - answer.num2;
-        console.log('The answer for the subtraction is: ', result);
+        console.log(chalk.blue('The answer for the subtraction is: '), chalk.blue(result));
         break;
     case '*':
         result = answer.num2 * answer.num2;
-        console.log('The answer for the multiplication is: ', result);
+        console.log(chalk.red('The answer for the multiplication is: '), chalk.red(result));
         break;
     case '/':
         result = answer.num1 / answer.num2;
-        console.log('The answer for the division is: ', result);
+        console.log(chalk.green('The answer for the division is: '), chalk.green(result));
         break;
 }
+// async function startAgain() {
+//     do{
+//         await answer();
+//        var again= await inquirer
+//         .prompt ({
+//             type:'input',
+//             name:'restart',
+//             message:'Do you want to continue again? press y or n'
+//         })
+//     }while(again.restart=='Y'|| again.restart=='y'|| again.restart=='yes'||again.restart=='Yes')
+// }
+// startAgain();
--- Assignments/Assignment07.ts
+++ Assignments/Assignment07.ts
@@ -4,6 +4,10 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+console.log(chalk.gray.bold.italic.underline('Calculator'));
+
+
+
 const answer = await inquirer.prompt([
     {
         type: 'number',
@@ -13,7 +17,7 @@ const answer = await inquirer.prompt([
     {
         type: 'number',
         name: 'num2',
-        message: 'Enter the second number'
+        message: 'Enter the second number: '
     },
     {
         type: 'list',
@@ -26,18 +30,31 @@ let result: number;
 switch (answer.operator) {
     case '+':
         result = answer.num1 + answer.num2;
-        console.log('The answer for addition is: ', result);
+        console.log(chalk.yellow('The answer for addition is: '), chalk.yellow(result));
         break;
     case '-':
         result = answer.num1 - answer.num2;
-        console.log('The answer for the subtraction is: ', result);
+        console.log(chalk.blue('The answer for the subtraction is: '), chalk.blue(result));
         break;
     case '*':
         result = answer.num2 * answer.num2;
-        console.log('The answer for the multiplication is: ', result);
+        console.log(chalk.red('The answer for the multiplication is: '), chalk.red(result));
         break;
     case '/':
         result = answer.num1 / answer.num2;
-        console.log('The answer for the division is: ', result);
+        console.log(chalk.green('The answer for the division is: '), chalk.green(result));
         break;
 }
+// async function startAgain() {
+//     do{
+//         await answer();
+//        var again= await inquirer
+//         .prompt ({
+//             type:'input',
+//             name:'restart',
+//             message:'Do you want to continue again? press y or n'
+//         })
+//     }while(again.restart=='Y'|| again.restart=='y'|| again.restart=='yes'||again.restart=='Yes')
+// }
+// startAgain();
+
