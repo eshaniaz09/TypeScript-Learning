25252d56b17bbbdf35119097b47c23bd8eb063de
Eesha Niaz <eshaniaz5@gmail.com>
2024-01-23 23:08:46 +0500

    updated

--- 13-time.ts
+++ 13-time.ts
@@ -2,7 +2,6 @@ import object1 from "./12-object";
 console.log(object1);
 
 
-// setTimeout() is an asynchronous function, meaning that the timer function will not pause execution of other functions in the functions stack.
 const login = (Homepage: () => void) => {
     console.log('loader start');
     
